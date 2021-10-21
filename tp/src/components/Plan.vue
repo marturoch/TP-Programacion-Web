6581466b722e24396794e951a38ffe2fcb813d8e
Isabella Paats <isabellapaats@hotmail.com>
2021-10-21 14:35:39 -0300

    planes

---
+++ tp/src/components/Plan.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="main">
+    <h2 class="planes-title">{{name}}</h2>
+    <p class="planes-tipo">{{tipo}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Plan",
+  props: [
+    "name",
+    "tipo"
+  ],
+}
+</script>
+
+<style scoped>
+
+</style>
--- tp/src/components/Planes.vue
+++ tp/src/components/Planes.vue
@@ -2,16 +2,42 @@
   <div>
     <header></header>
     <h1>Planes</h1>
+    <div class="planes-container">
+      <Plan v-for="(plan, index) in planes" v-bind:key="index"
+                    v-bind:name="plan.name"
+                    v-bind:image="plan.image"
+                    v-bind:tipo="plan.tipo"
+                    v-bind:item1="plan.item1"
+                    v-bind:item2="plan.item2"
+                    v-bind:item3="plan.item3">
+      </Plan>
+    </div>
     <Footer></Footer>
   </div>
 </template>
 
 <script>
+
+import Plan from "@/components/Plan";
+import {planes} from "@/assets/js/planes";
+
 export default {
-  name: "Planes",
+  name: "Planes.vue",
+  components: {
+    Plan
+  },
+  data(){
+    return {
+      planes: planes
+    }
+  }
 }
 </script>
 
 <style scoped>
-
+.planes-container{
+  display:flex;
+  justify-content: space-evenly;
+  flex-wrap: wrap;
+}
 </style>
