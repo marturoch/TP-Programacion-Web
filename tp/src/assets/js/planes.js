25577789c6eae9e29afdf4ea8a5a0bed6e1a6dbd
Isabella Paats <isabellapaats@hotmail.com>
2021-10-21 11:27:09 -0300

    planes

---
+++ tp/src/assets/js/planes.js
@@ -0,0 +1,11 @@
+const planes = [
+    {
+        name: "Frida",
+        image: x,
+        tipo: "Básico",
+        item1: ,
+        item2: ,
+        item3: ,
+        precio: "$115/día"
+    }
+]
