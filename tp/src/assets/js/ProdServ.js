059a4300ec359b65e479d484db40d04d8eb5fed4
Isabella Paats <isabellapaats@hotmail.com>
2021-11-01 21:56:15 -0300

    productos

--- tp/src/assets/js/ProdServ.js
+++ tp/src/assets/js/ProdServ.js
@@ -1,22 +1,27 @@
 const productos = [
     {
-        name:"medicamentos",
-        image:"medicamentos.jpg",
+        name:"SNACKS",
+        image:"snack_ppal.jpeg",
         description:"descripcion"
     },
     {
-        name:"alimentos",
-        image:"alimentos.jpg",
+        name:"ALIMENTOS",
+        image:"alimento_ppal.jpeg",
         description:"descripcion"
     },
     {
-        name:"Vacunas",
-        image:"vacuna.png",
+        name:"JUGUETES",
+        image:"juguetes_ppal.jpeg",
         description:"descripcion"
     },
     {
-        name:"PetFind",
+        name:"PETFIND",
         image:"petfind.jpg",
+        description:"descripcion"
+    },
+    {
+        name:"HIGIENE",
+        image:"higiene_ppal.png",
         description:"descripcion"
     }
 ]
@@ -37,7 +42,214 @@ const servicios = [
         description:"descripcion"
     }
 ]
+
+const alimentos = [
+    {
+        name: "ROYAL CANIN MAXI ADULT DERMACONFORT",
+        image: "ROYAL-CANIN-MAXI-ADULT-DERMACONFORT.png",
+        price: 5690,
+    },
+    {
+        name: "ROYAL CANIN MAXI ADULT STERILIZED",
+        image: "ROYAL-CANIN-MAXI-ADULT-STERILIZED.png",
+        price: 5390,
+    },
+    {
+        name: "ROYAL CANIN MAXI ADULT WEIGHT CARE",
+        image: "Royal-Canin-maxi-adult-weight-care.png",
+        price: 5690,
+    },
+    {
+        name: "ROYAL CANIN MEDIUM ADULT DERMACONFORT",
+        image: "ROYAL-CANIN-MEDIUM-ADULT-DERMACONFORT.png",
+        price: 5690,
+    },
+    {
+        name: "ROYAL CANIN MEDIUM ADULT STERILIZED",
+        image: "ROYAL-CANIN-MEDIUM-ADULT-STERILIZED.png",
+        price: 5690,
+    },
+    {
+        name: "ROYAL CANIN MEDIUM ADULT WEIGHT CARE",
+        image: "Royal-Canin-medium-adult-weight-care.png",
+        price: 5690,
+    },
+    {
+        name: "PRO PLAN ACTIVE MIND RAZAS MEDIANAS Y GRANDES",
+        image: "Pro-Plan-Active-Mind-Razas-Medianas-y-Grandes.png",
+        price: 6790,
+    },
+    {
+        name: "PRO PLAN ADULTO RAZAS GRANDES",
+        image: "Pro-Plan-Adulto-Raza-Grande.png",
+        price: 6190,
+    },
+    {
+        name: "PRO PLAN ADULTO RAZAS MEDIANAS",
+        image: "Pro-Plan-Adulto-Razas-Medianas.png",
+        price: 6190,
+    },
+    {
+        name: "PRO PLAN ADULTO RAZAS PEQUEÑAS",
+        image: "Pro-Plan-Adulto-Razas-Pequenas.png",
+        price: 3790,
+    },
+    {
+        name: "PRO PLAN PUPPY SMALL BREED",
+        image: "Pro-Plan-Puppy-Small-Breed.png",
+        price: 4120,
+    },
+    {
+        name: "PRO PLAN PUPPY LARGE BREED",
+        image: "Pro-Plan-Puppy-Large-Breed-1.png",
+        price: 6790,
+    },
+    {
+        name: "PRO PLAN PUPPY COMPLETE",
+        image: "Pro-Plan-Puppy-Medium-Breed.png",
+        price: 6790,
+    },
+]
+
+const snacks = [
+    {
+        name: "DONA CHICA",
+        image: "dona_chica.png" ,
+        price: 110,
+    },
+    {
+        name: "HUESO DE CUERO",
+        image: "hueso-de-cuero.png" ,
+        price: 380,
+    },
+    {
+        name: "MINI ROLL (10 u)",
+        image: "mini-roll.png" ,
+        price: 600,
+    },
+    {
+        name: "PEDIGREE RODEO SABOR CARNE",
+        image: "Renders-Rodeo-CARNE.png" ,
+        price: 140,
+    },
+    {
+        name: "PEDIGREE RODEO SABOR POLLO",
+        image: "Renders-Rodeo-POLLO.png" ,
+        price: 140,
+    },
+    {
+        name: "BISCUITS DE POLLO (120gr)",
+        image: "Biscuits-de-Pollo-x-500.png" ,
+        price: 130,
+    },
+    {
+        name: "CUIDADO DENTAL DIARIO DENTAL CLEAN (400gr)",
+        image: "Cuidado-Dental-Diario-Dental-Clean.png" ,
+        price: 640,
+    },
+    {
+        name: "CUIDADO DENTAL DIARIO DENTAL MILK STICKS (400gr)",
+        image: "Cuidado-Dental-Diario-Dental-Milk-Sticks.png" ,
+        price: 640,
+    },
+    {
+        name: "CUIDADO DENTAL DIARIO DENTAL STICKS (400gr)",
+        image: "Cuidado-Dental-Diario-Dental-Sticks.png" ,
+        price: 640,
+    },
+    {
+        name: "SNACK SALUDABLE DE POLLO GRILLÉ (400gr)",
+        image: "Snack-Saludable-de-Pollo-Grille.png" ,
+        price: 800,
+    }
+]
+
+const juguetes = [
+    {
+        name: "juguete para perros KONG Classic",
+        image: "kong-classic.png" ,
+        price: 680
+    },
+    {
+        name: "Juguete KONG Extreme",
+        image: "kong-extreme.png" ,
+        price: 680
+    },
+    {
+        name: "Juguetes KONG Cachorros/Puppy",
+        image: "kong-cachorrospuppy.png" ,
+        price: 680
+    },
+    {
+        name: "Pelota KONG Ball",
+        image: "kong-ball.png" ,
+        price: 560
+    },
+    {
+        name: "Hueso KONG Puppy/Cachorro",
+        image: "kong-puppycachorro.png" ,
+        price: 790
+    },
+    {
+        name: "Ice - Aro Congelado",
+        image: "ice-aro-congelado.png" ,
+        price: 1420
+    },
+    {
+        name: "Soga 5 Nudos",
+        image: "soga-nudos.png" ,
+        price: 1580
+    },
+]
+
+const higiene = [
+    {
+        name: "SHAMPOO A BASE DE ALOE & AVENA",
+        image: "Shampoo-a-base-de-Aloe-Avena.png",
+        price: 340
+    },
+    {
+        name: "SHAMPOO A BASE DE PEROXIDO DE BENZOLIO",
+        image: "Shampoo-a-base-de-Peroxido-de-Benzolio.png",
+        price: 390
+    },
+    {
+        name: "SHAMPOO ANTISEBORREICO",
+        image: "Shampoo-Antiseborreico.png",
+        price: 340
+    },
+    {
+        name: "SHAMPOO ANTISEPTICO PH CONTROLADO PARA CACHORROS",
+        image: "Shampoo-Antiseptico-ph-Controlado-para-Cachorros.png",
+        price: 260
+    },
+    {
+        name: "SHAMPOO CON ALGAS VITALIZADOR ABRILLANTADOR",
+        image: "Shampoo-con-Algas.png",
+        price: 330
+    },
+    {
+        name: "SHAMPOO AQUA ECTO GARRAPATICIDA",
+        image: "Shampoo-con-Ectol-Garrapaticida.png",
+        price: 330
+    },
+    {
+        name: "SHAMPOO DOUBLE CON ENJUAGUE 2 EN 1",
+        image: "Shampoo-con-Enjuague-2-en-1.png",
+        price: 340
+    },
+    {
+        name: "SHAMPOO CON GLICERINA",
+        image: "Shampoo-con-Glicerina.png",
+        price: 260
+    }
+]
+
 export{
     productos,
-    servicios
+    servicios,
+    alimentos,
+    snacks,
+    juguetes,
+    higiene
 }
