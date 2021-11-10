1724cd5200a54d0b53f716fedaa780e6f8c97122
Valentina Kelly <valentinakelly002@gmail.com>
2021-11-09 23:44:16 -0300

    commit

--- tp/src/components/Main.vue
+++ tp/src/components/Main.vue
@@ -1,6 +1,12 @@
 <template>
-  <div id="main">
+  <div id="nosotros">
     <h1>Nosotros</h1>
+    <div id="info">
+    <h3>DogCare asegura la salud y el cuidado de tu perro. Está dirigido a aquellas personas que cuidan a sus mascotas y quieren estar cubiertos ante cualquier imprevisto que pueda ocurrirle</h3>
+    <h2>Cuidá a tu mejor amigo como él se lo merece, Aseguralo en DogCare.</h2>
+    <h3>Es decir, si un accidente le ocurriera a tu mascota, vos y tu familia disponen de los medios necesarios para hacer frente a cualquier tipo de intervención por parte de un médico veterinario. También, ante una lamentable pérdida de la mascota, este seguro posibilita obtener una indemnización para adoptar otro animal.</h3>
+
+      </div>
   </div>
 </template>
 
@@ -11,6 +17,33 @@ export default {
 </script>
 
 <style scoped>
-@import "../assets/css/main.css";
+
+h1 {
+  text-transform: uppercase;
+  color: black;
+  font-size: 60px;
+}
+
+#info {
+  color: black;
+  text-align: center;
+  font-weight: bold;
+  font-size: 15px;
+  padding-left: 80px;
+  padding-right: 80px;
+  border:4px solid #820263;
+  margin-left: 200px;
+  width: 800px;
+  height: auto;
+}
+
+h2 {
+  text-transform: uppercase;
+  color: #A82885;
+  font-family: 'Permanent Marker';
+  font-size: 20px;
+  padding-left: 10px;
+}
+
 </style>
 
