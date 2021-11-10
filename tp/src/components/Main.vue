cb892a1896225579e64f68d423bc6ed090899a05
nikiknak <nicoleknak11@gmail.com>
2021-11-10 01:00:21 -0300

    commit

--- tp/src/components/Main.vue
+++ tp/src/components/Main.vue
@@ -2,17 +2,26 @@
   <div id="nosotros">
     <h1>Nosotros</h1>
     <div id="info-main">
-    <h3 class="info-main-h3">DogCare asegura la salud y el cuidado de tu perro. Está dirigido a aquellas personas que cuidan a sus mascotas y quieren estar cubiertos ante cualquier imprevisto que pueda ocurrirle</h3>
-    <h2 class="info-main-h2">Cuidá a tu mejor amigo como él se lo merece, Aseguralo en DogCare.</h2>
-    <h3 class="info-main-h3">Es decir, si un accidente le ocurriera a tu mascota, vos y tu familia disponen de los medios necesarios para hacer frente a cualquier tipo de intervención por parte de un médico veterinario. También, ante una lamentable pérdida de la mascota, este seguro posibilita obtener una indemnización para adoptar otro animal.</h3>
+    <h3 class="info-main-h3">{{info1}}</h3>
+    <h2 class="info-main-h2">{{info2}}</h2>
+    <h3 class="info-main-h3">{{info3}}</h3>
     </div>
   </div>
 </template>
 
 <script>
 export default {
-  name: "Main"
+  name: "Main",
+  data () {
+    return {
+      info1:"DogCare asegura la salud y el cuidado de tu perro. Está dirigido a aquellas personas que cuidan a sus mascotas y quieren estar cubiertos ante cualquier imprevisto que pueda ocurrirle",
+      info2:"¡Cuidá a tu mejor amigo como él se lo merece, Aseguralo en DogCare!",
+      info3:"Es decir, si un accidente le ocurriera a tu mascota, vos y tu familia disponen de los medios necesarios para hacer frente a cualquier tipo de intervención por parte de un médico veterinario. También, ante una lamentable pérdida de la mascota, este seguro posibilita obtener una indemnización para adoptar otro animal."
+    }
+  }
+
 }
+
 </script>
 
 <style scoped>
